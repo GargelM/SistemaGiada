88f71d4a0d9a5b3772fda66979f132c0e369f95e
Michel Gargel <gargelmichel@gmail.com>
2018-06-07 20:47:55 -0300

    Criando a classe de conexão

---
+++ SistemaGiada/src/br/com/SistemaGiada/util/HibernateUtil.java
@@ -0,0 +1,34 @@
+package br.com.SistemaGiada.util;
+
+import javax.imageio.spi.ServiceRegistry;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static final SessionFactory sessionFactory = buildSessionFactory(); //iniciar conexao
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			// Create the SessionFactory from hibernate.cfg.xml
+			//return new Configuration().configure().buildSessionFactory(new StandardServiceRegistryBuilder().build());
+			
+			Configuration configuration = new Configuration();
+			configuration.configure();
+			
+			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySetting(configuration.getProperties()).value);
+			
+		} catch (Throwable ex) {
+			//Mensagem de erro ao conectar
+			System.err.println("Erro na conxexão." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+}
