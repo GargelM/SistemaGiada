213e278f628fcfa85bb33975954be4c0b953fc4c
Michel Gargel <gargelmichel@gmail.com>
2018-06-13 20:14:18 -0300

    Metodo para salvar no hibernate

--- SistemaGiada/src/br/com/SistemaGiada/DAO/FornecedoresDAO.java
+++ SistemaGiada/src/br/com/SistemaGiada/DAO/FornecedoresDAO.java
@@ -1,15 +1,33 @@
 package br.com.SistemaGiada.DAO;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import br.com.SistemaGiada.domain.Fornecedor;
 import br.com.SistemaGiada.util.HibernateUtil;
 
 public class FornecedoresDAO {
-	public void salvar(Fornecedor fornecedor) {
+	public void salvar(Fornecedor fornecedor) throws Exception{
 		Session sessao = HibernateUtil.getSessionFactory().openSession();//iniciando a sessao conexao criada exemplo fabrica..
 		
-		sessao.save(fornecedor);
-		sessao.close();
+		Transaction transacao = null;
+		
+		try {
+			transacao = sessao.beginTransaction();//abrindo a transacao
+			transacao.commit(); // confirmando a transação
+			
+			sessao.save(fornecedor);//salvando os dados
+			
+		}catch (Exception e) {
+			if(transacao != null) {
+				transacao.rollback(); //desfaz a transação
+				sessao.close();
+			}
+			
+			throw e;
+		}finally {
+			
+		}
+
 	}
 }
