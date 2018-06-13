858f7fc7248510d3fe5eb2af5569afd3011ff992
Michel Gargel <gargelmichel@gmail.com>
2018-06-13 20:04:36 -0300

    dao

--- SistemaGiada/src/br/com/SistemaGiada/DAO/FornecedoresDAO.java
+++ SistemaGiada/src/br/com/SistemaGiada/DAO/FornecedoresDAO.java
@@ -1,5 +1,15 @@
 package br.com.SistemaGiada.DAO;
 
-public class FornecedoresDAO {
+import org.hibernate.Session;
 
+import br.com.SistemaGiada.domain.Fornecedor;
+import br.com.SistemaGiada.util.HibernateUtil;
+
+public class FornecedoresDAO {
+	public void salvar(Fornecedor fornecedor) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();//iniciando a sessao conexao criada exemplo fabrica..
+		
+		sessao.save(fornecedor);
+		sessao.close();
+	}
 }
